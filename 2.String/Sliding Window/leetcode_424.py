c219dabd834b9d4a5b22ec69440efc4f2cfe2a3b
Pratik3186 <pratikk.3186@gmail.com>
2026-06-14 16:36:35 +0530

    fixed static method leetcode 424

--- 2.String/Sliding Window/leetcode_424.py
+++ 2.String/Sliding Window/leetcode_424.py
@@ -1,20 +1,30 @@
 from collections import defaultdict
-class solution:
-    def characterReplacement(k,s):
+
+
+class Solution:
+    @staticmethod
+    def characterReplacement(s: str, k: int) -> int:
         left = 0
         freq = defaultdict(int)
         max_freq = 0
         ans = 0
+
         for right in range(len(s)):
             freq[s[right]] += 1
             max_freq = max(max_freq, freq[s[right]])
-            while (right-left+1)-max_freq>k:
+
+            while (right - left + 1) - max_freq > k:
                 freq[s[left]] -= 1
-                left +=1
-            ans = max(ans,right-left+1)
+                left += 1
+
+            ans = max(ans, right - left + 1)
+
         return ans
-    
-s = 'AABA'
-k = 2
-result = solution.characterReplacement(k,s)
-print(result)
+
+
+if __name__ == "__main__":
+    s = "AABA"
+    k = 2
+    result = Solution.characterReplacement(s, k)
+    print(result)
+
