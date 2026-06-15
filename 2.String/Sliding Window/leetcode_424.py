651b2a32f4bf63c265b581b5d226f06194c3a296
Pratik3186 <pratikk.3186@gmail.com>
2026-06-15 16:04:32 +0530

    leetcode 438

--- 2.String/Sliding Window/leetcode_424.py
+++ 2.String/Sliding Window/leetcode_424.py
@@ -1,6 +1,4 @@
 from collections import defaultdict
-
-
 class Solution:
     @staticmethod
     def characterReplacement(s: str, k: int) -> int:
@@ -8,20 +6,14 @@ class Solution:
         freq = defaultdict(int)
         max_freq = 0
         ans = 0
-
         for right in range(len(s)):
             freq[s[right]] += 1
             max_freq = max(max_freq, freq[s[right]])
-
             while (right - left + 1) - max_freq > k:
                 freq[s[left]] -= 1
                 left += 1
-
             ans = max(ans, right - left + 1)
-
         return ans
-
-
 if __name__ == "__main__":
     s = "AABA"
     k = 2
