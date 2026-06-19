538e87b2daad33ff05ce5e43b89bf39fb80d0ba8
Pratik3186 <pratikk.3186@gmail.com>
2026-06-20 00:17:05 +0530

    leetcode_1

---
+++ 2.String/Two Pointer /1.Opposite_direction_Pointer/leetcode_1.py
@@ -0,0 +1,16 @@
+#Two_Sum
+class Solution:
+    @staticmethod
+    def twosum(nums,target):
+        num_map = {}
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement],i]
+            num_map[num] = i
+
+if __name__ == '__main__':
+    nums = [2,7,11,15]
+    target = 9
+    result = Solution.twosum(nums,target)
+    print(result)
