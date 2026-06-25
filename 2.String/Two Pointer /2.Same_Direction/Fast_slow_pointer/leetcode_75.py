ab72d062bfc8af5b129310b955de0bef01e28d34
Pratik3186 <pratikk.3186@gmail.com>
2026-06-26 01:45:35 +0530

    Leetcode_75

---
+++ 2.String/Two Pointer /2.Same_Direction/Fast_slow_pointer/leetcode_75.py
@@ -0,0 +1,22 @@
+class Solution:
+    @staticmethod
+    def sortColor(nums):
+        low = 0
+        mid = 0
+        high = len(nums)-1
+        while mid<=high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid],nums[low]
+                mid+=1
+                low+=1
+            elif nums[mid] == 1:
+                mid +=1
+            else:
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high -=1
+        return nums
+            
+if __name__ == '__main__':
+    nums = [2,0,1,0,1,2,1,1,2,0]
+    result = Solution.sortColor(nums)
+    print(result)
